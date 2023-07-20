c6d1c282a07e74d246a7e5b9a57e7ed32c291be2
Davi Costa <davifncosta@gmail.com>
2023-07-20 14:41:50 -0300

    navbar

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,11 +1,13 @@
 import { AddShoppingCart, Favorite } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./FeaturedProducts.scss";
 
 const FeaturedProducts = ({ type, description, productsMap, showTrending }) => {
   const [itemCard, setItemCard] = useState([]);
   const [featuredProducts, setFeaturedProducts] = useState([]);
   const [treadingProducts, setTreadingProducts] = useState([]);
+  const categoryItems = ["accessories", "children", "men", "woman"]
 
   useEffect(() => {
     const getItemCard = Object.values(productsMap);
@@ -19,6 +21,12 @@ const FeaturedProducts = ({ type, description, productsMap, showTrending }) => {
     setTreadingProducts(trendingItemsMap);
   }, [productsMap]);
 
+ const productsToRender = showTrending
+    ? treadingProducts.map((array) => array[0])
+    : featuredProducts.map((array) => array[0]);
+
+  console.log(itemCard)
+
   const stylesIcons = {
     border: "1px solid #34435C64",
     padding: "0.5rem",
@@ -27,8 +35,7 @@ const FeaturedProducts = ({ type, description, productsMap, showTrending }) => {
     fontSize: "3.1rem",
   };
 
-  const productsToRender = showTrending ? treadingProducts.map(array => array[0]) : featuredProducts.map(array => array[0]);
-
+ 
   return (
     <div className="featured-products">
       <div className="top">
@@ -37,12 +44,16 @@ const FeaturedProducts = ({ type, description, productsMap, showTrending }) => {
       </div>
       <div className="bottom">
         {productsToRender.length > 0 &&
-          productsToRender.map((item) => (
+          productsToRender.map((item) => 
+          (
             <div key={item.id} className="product-card">
-              <img src={item.imageUrl} alt="imgCard" />
+            <Link to={`/product/${item.id}`}>
+                <img src={item.imageUrl} alt="imgCard" />
+              </Link>
               <div className="product-card-text">
                 <div>
                   <h2>{item.name}</h2>
+                
                   {item.offer.available ? (
                     <div className="price-with-sale">
                       <h2 className="old-price">$ {item.price}</h2>
